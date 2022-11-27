16bfe02ca52ff75710709e193ff55c5f618bfdc8
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-11-27 14:50:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+
+function writeCards(name , event) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+
+
+writeCards(names,"surprise")
+
+const countDown = function(n){
+    let i = n;
+    while(i >=0){
+        console.log(i)
+        i--
+    }
+}
